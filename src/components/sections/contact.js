96cf69ca9cd449d232767730f1fa2bd4069676b5
jjarrett21 <james.jarrett@okstate.edu>
2020-02-25 00:23:36 -0600

    places form in a section component

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react"
 import { Global, css } from "@emotion/core"
 import styled from "@emotion/styled"
+import ContactForm from "../form"
+
+const Container = styled.div`
+  margin: 0;
+  display: flex;
+  column-count: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+`
 
 const Contact = () => {
-	return (
-		<div>
-			<h2>Contact Section</h2>
-		</div>
-	)
+  return (
+    <>
+      <Container>
+        <ContactForm />
+      </Container>
+    </>
+  )
 }
 
 export default Contact
