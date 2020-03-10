22edf58cdf4eafbd2b1b40531cda215a047b6467
jjarrett21 <jjarrett@DESKTOP-DRNL2ON.localdomain>
2020-03-10 00:45:29 -0500

    just testing my config settings

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -39,6 +39,12 @@ const projectData = [
     icon: "",
     link: "https://github.com/skyylife/skyylife.io",
   },
+  {
+    id: 1,
+    name: "Skyy Life Website",
+    icon: "",
+    link: "https://github.com/skyylife/skyylife.io",
+  },
 ]
 
 const Projects = () => {
