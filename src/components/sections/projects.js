924bb421f43844e6cf8484afba6ca847b868bccc
jjarrett21 <jjarrett@DESKTOP-DRNL2ON.localdomain>
2020-03-09 20:36:23 -0500

    adds a test comment

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react"
 import { Global, css } from "@emotion/core"
 import styled from "@emotion/styled"
 
+// This is a test comment
+
 const Projects = () => {
-	return (
-		<div>
-			<h2>Projects Section</h2>
-		</div>
-	)
+  return (
+    <div>
+      <h2>Projects Section</h2>
+    </div>
+  )
 }
 
 export default Projects
