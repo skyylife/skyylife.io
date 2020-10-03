4287225e6f7dd52fd88843213911ada24c57ec2a
jjarrett21 <james.jarrett@okstate.edu>
2020-10-03 01:12:31 -0500

    gets card on nth side

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,9 +1,8 @@
 /** @jsx jsx */
 import React from "react"
 import { jsx, css } from "@emotion/core"
-import { Card, Container } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 import { headerStyles } from "../../styles/shared-styles"
-import { Jumbotron } from "react-bootstrap"
 import { Element } from "react-scroll"
 import jamesAv from "../../images/James.png"
 import dymonAv from "../../images/Dymon.png"
@@ -19,35 +18,34 @@ const wrapperStyles = css`
 
 const aboutGridStyles = css`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(800px, 1000px));
-  grid-auto-rows: minmax(200px, 300px);
-  left: 0;
+  grid-auto-rows: minmax(100px, 300px);
+  grid-template-columns: repeat(2, auto);
+  grid-auto-flow: dense;
+
   grid-gap: 30px;
-  lflex: 1 0 auto;
+  flex: 1 0 auto;
   margin: 1rem;
   padding: 0.5em;
+
+  &:nth-of-type(even) {
+    justify-content: flex-end;
+    grid-column: 1 / 2;
+  }
 `
 
 const cardStyles = url => css`
   display: flex;
   border: none;
-  margin: 0.5em;
-  padding: 1em;
-  width: auto;
+  margin: 0;
+  padding: 0;
+  width: 300px;
   background-image: url(${url});
   background-position: center center;
   background-color: transparent;
   background-size: auto 100%;
   background-repeat: no-repeat;
-  float: left;
-  flex-direction: row-reverse;
 
   .card-body {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
     height: 100%;
     width: auto;
     opacity: 0;
@@ -56,32 +54,16 @@ const cardStyles = url => css`
     justify-content: flex-start;
   }
 
-  .card-text {
-    color: white;
-    font-size: 20px;
-    position: relative;
-    text-align: center;
-  }
-
   .card-title {
     color: white;
     font-size: 40px;
     position: relative;
     text-align: center;
   }
-
-  &:hover .card-body {
-    opacity: 0.5;
-    cursor: pointer;
-  }
 `
-
-const jumbotronStyles = css`
-  margin-left: 400px;
-  width: 250px;
-  height: 250px;
-  background-color: transparent;
-  opacity: 1;
+const excerptStyles = css`
+  color: white;
+  font-size: 20px;
 `
 
 const userData = [
@@ -89,57 +71,60 @@ const userData = [
     id: 0,
     name: "James Jarrett",
     avatar: `${jamesAv}`,
-    excerpt:
-      "This is James, Oklahoma State Electrical Engineering Technology graduate, currently working as a software engineer for a startup. James has a passion for making technologies aceesible for all while pushing for edge and emergent technologies to find out more about james checkout his github(link) and linkedin(link) ",
+    excerpt: (
+      <p>
+        This is James, Oklahoma State Electrical Engineering Technology
+        graduate, currently working as a software engineer for a startup. James
+        has a passion for making technologies and opportunities accessible to
+        everyone.
+      </p>
+    ),
   },
   {
     id: 1,
     name: "Aaron Shepherd",
     avatar: `${aaronAv}`,
-    excerpt: "This is Aaron, the CTO",
+    excerpt: <p>This is Aaron</p>,
   },
 
   {
     id: 2,
     name: "Dymon Johnson",
     avatar: `${dymonAv}`,
-    excerpt: "This is Dymon, the CDO",
+    excerpt: <p>This is Dymon</p>,
   },
   {
     id: 3,
     name: "Quincey Thomas",
     avatar: `${quinceyAv}`,
-    excerpt: "This is Quincey, the CPO",
+    excerpt: <p>This is Quincey</p>,
   },
 
   {
     id: 4,
     name: "Sharrone Berry-Davis",
     avatar: `${sharroneAv}`,
-    excerpt: "This is Sharrone, the CFO",
+    excerpt: <p>This is Sharrone</p>,
   },
 ]
 
 const About = () => {
   return (
     <Element id="about" name="about" className="element">
-    <div css={wrapperStyles}>
-      <header css={headerStyles("flex-start")}>The High Council</header>
-      <div css={aboutGridStyles}>
+      <div css={wrapperStyles}>
+        <header css={headerStyles("flex-start")}>The High Council</header>
+
         {userData.map(u => (
-          <Card key={u.id} css={cardStyles(u.avatar)}>
-            <Card.Body>
-              <Card.Title>{u.name}</Card.Title>
-            </Card.Body>
-            <Jumbotron fluid css={jumbotronStyles}>
-              <Container>
-                <p>{u.excerpt}</p>
-              </Container>
-            </Jumbotron>
-          </Card>
+          <div css={aboutGridStyles}>
+            <Card key={u.id} css={cardStyles(u.avatar)}>
+              <Card.Body>
+                <Card.Title>{u.name}</Card.Title>
+              </Card.Body>
+            </Card>
+            <span css={excerptStyles}> {u.excerpt} </span>
+          </div>
         ))}
       </div>
-    </div>
     </Element>
   )
 }
